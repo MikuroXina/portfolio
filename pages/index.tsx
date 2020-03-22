84869eb6a3c6f5cef89a78ec2721a7f7f2e28358
MikuroXina <ryosukadnak@gmail.com>
2020-03-22 22:40:36 +0900

    Write boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,50 @@
 import {NextPage} from 'next';
 
-const Main: NextPage = () => <div>Hello, World!</div>;
+const Main: NextPage = () => (
+  <>
+    <h1>Mikuro Xina</h1>
+    <article>
+      <h2>About</h2>
+      <section>
+        <h3>Objective</h3>
+      </section>
+      <section>
+        <h3>Skills</h3>
+      </section>
+      <section>
+        <h3>Education</h3>
+      </section>
+    </article>
+    <article>
+      <h2>My Products</h2>
+      <section>
+        <h3>Programs</h3>
+      </section>
+      <section>
+        <h3>Musics</h3>
+      </section>
+      <section>
+        <h3>Books</h3>
+      </section>
+    </article>
+    <style jsx>{`
+      h1 {
+        color: #c7c7c7;
+      }
+      h2 {
+        color: #3794ff;
+      }
+      h3 {
+        color: #28b66f;
+      }
+    `}</style>
+    <style jsx global>{`
+      body {
+        background-color: #191d19;
+        color: #bbbbbb;
+      }
+    `}</style>
+  </>
+);
 
 export default Main;

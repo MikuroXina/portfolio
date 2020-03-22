1e3603d2866605f0393c39dbd6448ce76affb670
MikuroXina <ryosukadnak@gmail.com>
2020-03-22 23:25:01 +0900

    Write My Products

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,12 +51,87 @@ const Main: NextPage = () => (
       <h2>My Products</h2>
       <section>
         <h3>Programs</h3>
+        Recently:
+        <ul>
+          <li>
+            <cite>
+              <a href="https://github.com/MikuroXina/xdraw">xdraw</a>
+            </cite>{' '}
+            The WebGL 3D engine based on both three.js and Unity.
+          </li>
+          <li>
+            <cite>
+              <a href="https://github.com/MikuroXina/bms-bounce">bms-bounce</a>
+            </cite>{' '}
+            The bouncer, convert BMS to WAV.
+          </li>
+          <li>
+            <cite>
+              <a href="https://github.com/MikuroXina/lua-js">lua-js</a>
+            </cite>{' '}
+            The Lua binding for JavaScript.
+          </li>
+        </ul>
+        Old Games (they doesn't work well):
+        <ul>
+          <li>
+            <cite>
+              <a href="https://github.com/MikuroXina/Taiku-Ho-Ka">
+                Taiku-Ho-Ka
+              </a>
+            </cite>{' '}
+            The fixed point shooting game.
+          </li>
+          <li>
+            <cite>
+              <a href="https://github.com/MikuroXina/PullWall">PullWall</a>
+            </cite>{' '}
+            The dynamic maze.
+          </li>
+          <li>
+            <cite>
+              <a href="https://github.com/MikuroXina/CaseByeCase">
+                CaseByeCase
+              </a>
+            </cite>{' '}
+            The puzzle inspired by Machinarium.
+          </li>
+        </ul>
       </section>
       <section>
         <h3>Musics</h3>
+        <iframe
+          width="320"
+          height="160"
+          src="https://ext.nicovideo.jp/thumb_user/40015175"
+          scrolling="no"
+          style={{border: 'solid 1px #CCC;'}}
+          frameBorder={0}
+        ></iframe>
       </section>
       <section>
         <h3>Books</h3>
+        <ul>
+          <li>
+            <cite>
+              <a href="https://github.com/MikuroXina/cpp-book">cpp-book</a>
+            </cite>{' '}
+            The C++ tutorial book in Japanese.
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h3>Miscs</h3>
+        <ul>
+          <li>
+            <cite>
+              <a href="https://github.com/MikuroXina/cherry-petals-theme">
+                cherry-petals-theme
+              </a>
+            </cite>{' '}
+            My favorite theme for VSCode.
+          </li>
+        </ul>
       </section>
     </article>
     <style jsx>{`

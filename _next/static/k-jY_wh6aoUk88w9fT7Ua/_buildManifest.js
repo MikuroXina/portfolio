d44e07b952eaa538c7e960c3dd56d2adbbb18a88
MikuroXina <MikuroXina@users.noreply.github.com>
2021-04-13 13:03:35 +0000

    deploy: 21f13acf1e7a8c5356b5e18d65e36189a7d3a586

--- _next/static/k-jY_wh6aoUk88w9fT7Ua/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-d682b23de2147f3191e4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cd1c05ff47f7c8c843ff.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

4c2689a7c57345147b42699cfa523cc22cdf6b52
MikuroXina <ryosukadnak@gmail.com>
2023-05-04 00:09:14 +0900

    Rename as organisms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { About } from '../components/about';
+import { About } from '../organisms/about';
 import Head from 'next/head';
 import type { NextPage } from 'next';
-import { Skills } from '../components/skills';
+import { Skills } from '../organisms/skills';
 import style from './index.module.css';
-import { Vision } from '../components/vision';
-import { Projects } from '../components/projects';
+import { Vision } from '../organisms/vision';
+import { Projects } from '../organisms/projects';
 
 const Main: NextPage = () => (
   <div className={style.container}>

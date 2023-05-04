31a60a8242f3ebc6fa557102e225549d469c98a8
MikuroXina <ryosukadnak@gmail.com>
2023-05-04 11:54:51 +0900

    Specify defaultLocale

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '/'
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '/',
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
 };
